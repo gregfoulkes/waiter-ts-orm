d9ff112e714bb6ea951c31897099b09d046386e7
gregfoulkes <greg_foulkes@live.com>
2018-10-05 14:46:27 +0200

    changed assign shift gunctionality to work by id

--- sql-scripts/insert-days.sql
+++ sql-scripts/insert-days.sql
@@ -1,3 +1,9 @@
+drop table day cascade;
+create table day (
+	id serial not null primary key,
+    dayname varchar(20)
+);
+
 insert into day (dayname) values('Monday');
 insert into day (dayname) values('Tuesday');
 insert into day (dayname) values('Wednesday');
